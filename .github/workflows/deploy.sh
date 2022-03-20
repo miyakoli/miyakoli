981d67d83d6385bf242c5adf96217a6c07d8bc34
ryusukenarita-glpgs <ryusuke.narita@glpgs.com>
2022-03-20 15:27:27 +0900

    GitHub Actions - .github を deploy 内容から除外

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -32,7 +32,8 @@ done
 
 
 aws s3 sync . "${ADLP_ORIGIN_S3}" \
-    --delete --acl public-read --exclude "*.git*"
+    --delete --acl public-read \
+    --exclude "*.git*" --exclude "*.github*"
 aws cloudfront create-invalidation \
     --distribution-id "${ADLP_DISTRO_ID}" --paths "/*"
 
